4fc9f102797ba1616510ae5e09d8b6c10e8c28d0
Ricardo Olarte <raop155@hotmail.com>
2021-01-12 15:14:48 -0500

    Add formik validation

--- App.js
+++ App.js
@@ -3,17 +3,38 @@ import React from 'react';
 import { useFormik } from 'formik';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
+function validateEmail(email) {
+  return re.test(String(email).toLowerCase());
+}
+
+const validate = (values) => {
+  const errors = {};
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (!values.email) {
+    errors.email = 'Email required';
+  } else if (!re.test(values.email)) {
+    errors.email = 'Email invalid';
+  }
+  return errors;
+};
+
 export default function App() {
   const formik = useFormik({
     initialValues: {
       email: 'lala@lala.com',
     },
+    validate,
     onSubmit: (x) => console.warn(x),
   });
   return (
     <View style={styles.container}>
-      <Text>Email</Text>
-      <TextInput onChangeText={formik.handleChange('email')} value={formik.values.email} />
+      <Text>Enter email:</Text>
+      <TextInput
+        style={styles.input}
+        onChangeText={formik.handleChange('email')}
+        value={formik.values.email}
+      />
+      {formik.errors?.email ? <Text style={styles.error}>{formik.errors.email}</Text> : null}
       <Button title='Send' onPress={formik.handleSubmit} />
       <StatusBar style='auto' />
     </View>
@@ -26,5 +47,15 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  input: {
+    height: 30,
+    width: 150,
+    paddingHorizontal: 10,
+    backgroundColor: '#eeeeee',
+  },
+  error: {
+    color: 'red',
+    fontSize: 12,
   },
 });
